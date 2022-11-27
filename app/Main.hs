dd9fb89ee597e622085aa7b04d940e2328426589
Luis Vegas <luisvegasmor@gmail.com>
2022-11-27 15:29:32 -0400

    feat: parsing basic expressions

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,49 @@
 module Main where
 
+import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
-data Ast = Var String | Int Integer | Bool Bool | String String
+data AstNode = Var String | Int Integer | Bool Bool | Binary AstNode AstNode | String String deriving (Eq)
 
-parseString :: Parser Ast
+eval :: AstNode -> AstNode
+eval value@(String _) = value
+eval value@(Int _) = value
+eval value@(Bool _) = value
+eval value@(Binary a b) =
+    let (Int x) = eval a
+     in let (Int y) = eval b
+         in Int (x + y)
+
+parseString :: Parser AstNode
 parseString = do
     char '"'
     x <- many (noneOf "\"")
     char '"'
     return $ String x
+
+parseBinary :: Parser AstNode
+parseBinary = do
+    first <- parseNumber
+    op <- symbol
+    rest <- parseNumber
+    return (Binary first rest)
+
+parseVariable :: Parser AstNode
+parseVariable = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let name = first : rest
+    return $ case name of
+        "true" -> Bool True
+        "false" -> Bool False
+        _ -> Var name
+
+parseNumber :: Parser AstNode
+parseNumber = fmap (Int . read) $ many1 digit
+
+parseExpr :: Parser AstNode
+parseExpr = parseVariable <|> parseNumber <|> parseString
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -18,12 +51,19 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "bronson" input of
-    Left err -> "Whooops " ++ show err
-    Right val -> "Found val"
+readExpr :: String -> AstNode
+readExpr input = case parse parseExpr "bronson" input of
+    Left err -> String $ "Whooops " ++ show err
+    Right val -> val
+
+toString :: AstNode -> String
+toString (Var name) = name
+toString (String literal) = literal
+toString (Int x) = show x
+toString (Bool True) = "True"
+toString (Bool False) = "False"
+
+instance Show AstNode where show = toString
 
 main :: IO ()
-main = do
-    (expr : _) <- getArgs
-    putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
